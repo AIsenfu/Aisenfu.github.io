8c5b15bcc2511008932058ae8bbfd063b5acae25
AIsenfu <124285028+AIsenfu@users.noreply.github.com>
2023-04-11 10:04:39 +0800

    Add files via upload

---
+++ 23.4.11.py
@@ -0,0 +1,174 @@
+#x=['a','b','kkk','lll','ex']
+#for i,v in enumerate(x):
+ #   print('li   ',i+1,'gejjj',v)
+
+
+#s=0
+#for i in range(1,101):
+ #   s +=i
+#else:
+ #   print(s)
+
+
+#c=sum(range(1,101))
+#print(c)
+
+#for n in range(100,1,-1):
+ #   if n%2 == 0:
+  #      continue
+   # for i in range(3,int(n**0.5)+1,2):
+    #    if n%i == 0:
+     #       break
+    #else:
+     #   print(n)
+      #  break
+
+
+#digits=(1,2,3,4)
+#for i in range (1000):
+#    result=[]
+#    for i in digits:
+#        for j in digits:
+#            for k in digits:
+#                result.append(i*100+j*10+k)
+
+#for i in range(1000):
+ #   result = []
+  #  for i in digits:
+  #      i=i*100
+   #     for j in digits:
+    #        j=j*10
+     #       for k in digits:
+      #          result.append(i+j+k)
+
+
+
+
+"""score=[]
+while True:
+    x=input('输入成绩')
+    try:
+        score.append(float(x))
+    except:
+        print('不是合法成绩')
+    while True:
+        flag=input('继续输入吗(yes/no)').lower()
+        if flag not in ('yes','no'):
+            print('只能输入yes no')
+        else:
+            break
+    if flag=='no':
+        break
+print(sum(score)/len(score))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
